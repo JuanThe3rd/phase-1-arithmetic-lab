b95c1846d34d1d277a807e943a7f5ec57796e3fc
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-05-22 11:36:06 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Test 1
+const num1 = 2;
+const num2 = 31;
+const multiply = num1*num2;
 
-// Write your code here
+// Test 2
+const random = Math.floor(Math.random() * 10) + 1;
+
+//Test 3
+const num3 = 34;
+const num4 = 5;
+const mod = num3%num4;
+
+//Test 4
+const max = Math.max(20, 2, 3, 4, 5);
